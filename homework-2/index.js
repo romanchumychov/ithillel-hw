c4f2c576f10ecb3d041c8872f62de28438c4f521
Roman Chumychov <chumychov.roman@pdffiller.team>
2023-12-21 21:42:17 +0200

    Home work #2

---
+++ homework-2/index.js
@@ -0,0 +1,12 @@
+// String Task
+const strOne = prompt("What's your name?")
+const strTwo = prompt("What's your age")
+const strThree = prompt("Do you have a cat?")
+
+alert("Name: " + strOne + ", Age: " + strTwo + ", Cat: " + strThree);
+
+// Number Task
+const number = prompt("Enter a five-digit number");
+const result = number.split("").join(" ");
+
+alert(result);
