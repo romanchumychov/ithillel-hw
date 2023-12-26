5da4d48121aa96f5be320b0e122a554f19b69761
Roman Chumychov <chumychov.roman@pdffiller.team>
2023-12-26 18:15:45 +0200

    Home work #4

---
+++ homework-4/index.js
@@ -0,0 +1,12 @@
+const firstNum = Number(prompt("Please enter first number"));
+const secondNum = Number(prompt("Please enter second number"));
+
+const addition = firstNum + secondNum;
+const subtraction = firstNum - secondNum;
+const multiplication = firstNum * secondNum;
+const division = firstNum / secondNum;
+
+alert(`Addition result: ${addition}`);
+alert(`Subtraction result: ${subtraction}`);
+alert(`Multiplication result: ${multiplication}`);
+alert(`Division result: ${division}`);
