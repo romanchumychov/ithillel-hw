47adef7f46b19ede62f7b5f759496fcf39b4e441
Roman Chumychov <chumychov.roman@pdffiller.team>
2024-02-27 12:40:14 +0200

    hw 28 fix

--- homework-28/index.js
+++ homework-28/index.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
-const randomImageName = images[Math.floor(Math.random() * images.length)]
+const IMG_LENGTH = 9;
+const randomIndex = Math.floor(Math.random() * IMG_LENGTH) + 1;
 const img = document.createElement("img");
 
-img.src = `some/path/${randomImageName}`;
-img.alt = randomImageName;
+img.src = `./images/${randomIndex}.jpeg`;
+img.alt = `img thumb ${randomIndex}`;
 
 document.body.appendChild(img);
