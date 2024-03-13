dce058db67deba8a79706e86fda46b2c1a60a931
Roman Chumychov <chumychov.roman@pdffiller.team>
2024-03-13 20:32:54 +0200

    hw 33 - refactor

--- homework-33/index.js
+++ homework-33/index.js
@@ -1,14 +1,20 @@
 "use strict";
 
-const form = document.getElementById("search");
-const formTextField = form.querySelector("input[type=text]");
+const handleForm = () => {
+  const form = document.getElementById("search");
+  const formTextField = form.querySelector("input[type=text]");
 
-form.addEventListener("submit", function(e) {
-  e.preventDefault();
-  const inputValue = formTextField.value;
-  const hasProtocol = inputValue.substring(0, 4) === "http";
-  const href = hasProtocol ? inputValue : `https://${inputValue}`;
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const inputValue = formTextField.value;
+    const hasProtocol = inputValue.substring(0, 4) === "http";
+    const href = hasProtocol ? inputValue : `https://${inputValue}`;
 
-  alert(`Redirect to: ${href}`);
-  window.location.href = inputValue.includes("http") ? inputValue : `https://${inputValue}`;
+    alert(`Redirect to: ${href}`);
+    window.location.href = href;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  handleForm();
 });
